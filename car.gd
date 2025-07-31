839ddfbe0a621b3a79c3f1c561faab61f0b19284
Omar Anwar <anwrmr649@gmail.com>
2025-07-31 14:27:20 +0300

    make car

--- car.gd
+++ car.gd
@@ -1,22 +1,30 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction: float = 0
+@export var speed: float = 200
+@export var friction: float = 200
+@export var max_speed: float = 500
 
-@onready var car := %Car
-
-@export var drifting_speed: float
-@export var distance : float = 100:
-	set(value):
-		distance = value
-		if car: car.position.x = value
-
-#func _ready() -> void:
-	#distance = distance
+func _ready() -> void:
+	speed += friction
 
 func _physics_process(delta: float) -> void:
-	distance += drifting_speed*delta*direction
-	global_rotation += PI*delta
-	rotation_degrees = wrap(rotation_degrees, -180, 180)
+	velocity.y = -500
+	
+	velocity.x += direction*speed*delta
+	
+	var friction_direction: int = -sign(velocity.x)
+	velocity.x += friction*friction_direction*delta
+	if sign(velocity.x) == friction_direction:
+		velocity.x = 0
+	
+	velocity.x = clamp(velocity.x, -max_speed, max_speed)
+	
+	move_and_slide()
 
 func _unhandled_input(_event: InputEvent) -> void:
 	direction = Input.get_axis("ui_left", "ui_right")
+
+
+func _on_screen_exited() -> void:
+	position.y = 400
